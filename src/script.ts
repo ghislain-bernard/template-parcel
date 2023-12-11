da85d77b91e65a9f585278cd63a0afa8e911bcdd
Ghislain Bernard <ghislain.bernard@gmail.com>
2023-12-11 01:16:12 +0100

    Insert 'src/script.ts'

---
+++ src/script.ts
@@ -0,0 +1,5 @@
+// ------------------------------------------- ghislain.bernard@gmail.com ------------------------------------------- //
+const environment : string = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+//
+console.log('template-parcel [' + environment + ']');
+// ------------------------------------------- ghislain.bernard@gmail.com ------------------------------------------- //
