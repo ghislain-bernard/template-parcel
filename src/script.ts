24448b6c0d405fe4ba4e96a70aa26fa1ae1694c3
Ghislain Bernard <ghislain.bernard@gmail.com>
2025-05-02 01:41:16 +0200

    Update 'src/script.ts'

--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,6 @@
 // ------------------------------------------- ghislain.bernard@gmail.com ------------------------------------------- //
+import '@fontsource/inter/latin-400.css';
+//
 const environment : string = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
 //
 console.log('template-parcel [' + environment + ']');
